7d90acd3a79f130b73de03349cb173da6db4e3d3
Jesus <yisuscri0701@gmail.com>
2025-06-18 17:56:41 -0600

    Archivo envexample

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ dotenv.config({path: '.env'}
 )
 
 export const config = {
-    port: process.env.PORT || 4000,
-    dbUri: process.env.DB_URI || 'mongodb://localhost:27017/realstate',
+    port: process.env.PORT || 4000, //Intenta usar la variable de entorno PORT. Si no existe (por ejemplo, en desarrollo), usa el valor por defecto: 4000.
+    dbUri: process.env.DB_URI || 'mongodb://localhost:27017/realstate', 
     jwSecret: process.env.JWT_SECRET || 'your_jwt_secret'
 }
